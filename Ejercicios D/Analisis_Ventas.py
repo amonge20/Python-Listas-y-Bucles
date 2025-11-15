ee1a0d514b1f59429a32746a4dceaebe2910aa2d
amonge20 <2023_aitor.monge@iticbcn.cat>
2025-11-15 12:02:56 +0100

    Realizacion del ejercicio de ventas

--- Ejercicios D/Analisis_Ventas.py
+++ Ejercicios D/Analisis_Ventas.py
@@ -11,3 +11,28 @@
 # semana.
 # Pista 2: Puede que necesites una variable que lleve la cuenta del día de la semana actual y se
 # reinicie a cero cuando llegue al séptimo día.
+
+# Creamos las listas para el numero de ventas y los días de la semana
+ventas = [120, 80, 140, 200, 75, 100, 180, 220, 160, 110, 90, 120, 170, 190, 250, 300, 95, 110, 
+          140, 180, 200, 160, 120, 80, 170, 150, 210, 190, 230, 250]
+dias_semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+
+# Creamos una lista con las ventas de cada dia
+ventas_por_dia = [0,0,0,0,0,0,0]
+
+# Creamos una variable para el dia actual
+dia_actual = 0
+
+# Creacion del bucle para el dia actual
+for venta in ventas:
+    # Nos situamos en los dias de la semana
+    ventas_por_dia[dia_actual] += venta
+    
+    # Si el dia llega a domingo, se reinicia a lunes
+    dia_actual += 1
+    if dia_actual == 7:
+        dia_actual = 0
+    
+# Mostrar el resultado final
+for i in range(7):
+    print(dias_semana[i],":",ventas_por_dia[i],"€")
